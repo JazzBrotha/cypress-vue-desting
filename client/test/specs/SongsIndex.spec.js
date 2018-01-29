47153b614f1729423268854089a785470b3f0f59
JazzBrotha <mattiasostblom@protonmail.com>
2018-01-29 01:33:41 +0100

    Setup tests for songs index

---
+++ client/test/specs/SongsIndex.spec.js
@@ -0,0 +1,43 @@
+import { shallow, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Songs from '../../src/components/Songs/Index.vue'
+import sinon from 'sinon'
+import axios from 'axios'
+import moxios from 'moxios'
+import flushPromises from 'flush-promises'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('SongsIndex.vue', () => {
+  let store, state
+  state = {
+    isUserLoggedIn: true,
+    user: 'user@user.com',
+    token: 'random string'
+  }
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state
+    })
+    moxios.install()
+  })
+  afterEach(() => {
+    moxios.uninstall()
+  })
+  it('sets songs to null at mount', () => {
+    const wrapper = shallow(Songs, { store, localVue })
+    expect(wrapper.vm.songs).toBe(null)
+  })
+  it('contains the right amount of songs after axios call', (done) => {
+    const wrapper = shallow(Songs, { store, localVue })
+    const onFulfilled = sinon.spy()
+    wrapper.setProps({ songs: ['Float On', 'abc', 'asdf', 'zzzzzz', 'asdf', 'Float On!'] })
+    axios.get('songs').then(onFulfilled)
+    moxios.wait(() => {
+      expect(wrapper.vm.songs).toHaveLength(6)
+      done()
+    })
+  })
+})
