f011d42ddb9ee1167ac4837f7838998f65eb07d6
JazzBrotha <mattiasostblom@protonmail.com>
2018-01-31 00:33:09 +0100

    Add test for edit song

--- client/test/specs/EditSong.spec.js
+++ client/test/specs/EditSong.spec.js
@@ -1,28 +1,35 @@
 import EditSong from '../../src/components/EditSong.vue'
-import ViewSong from '../../src/components/ViewSong/Index.vue'
 import { shallow, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
-import VueRouter from 'vue-router'
+import moxios from 'moxios'
 
 const localVue = createLocalVue()
 
 localVue.use(Vuex)
-localVue.use(VueRouter)
 
 describe('EditSong.vue', () => {
-  let actions, store, router, routes
+  let actions, store
 
   beforeEach(() => {
-    routes = [
-      {
-        path: '/songs/:songId', name: 'song', component: ViewSong
-      }
-    ]
-    router = new VueRouter({ routes })
     store = new Vuex.Store({
-      state: {},
+      state: {
+        token: 'amionog24Gm4iwogwgRgrrsvs',
+        user: 'testing@gmail.com',
+        isUserLoggedIn: true,
+        route: {
+          name: 'song',
+          params: {
+            songId: '6'
+          },
+          path: '/songs/6'
+        }
+      },
       actions
     })
+    moxios.install()
+  })
+  afterEach(() => {
+    moxios.uninstall()
   })
   it('has created a hook for component', () => {
     shallow(EditSong, {
@@ -30,9 +37,33 @@ describe('EditSong.vue', () => {
         song: {}
       },
       store,
-      localVue,
-      router
+      localVue
     })
-    expect(typeof EditSong.mounted).toBe('function')
+    expect(typeof EditSong.methods.save).toBe('function')
+  })
+  it('should return error when all required fields are not filled in', async () => {
+    moxios.uninstall()
+    const wrapper = shallow(EditSong, {
+      propsData: {
+        song: {}
+      },
+      store,
+      localVue
+    })
+    const createBtn = wrapper.find('#edit-song-btn')
+    createBtn.trigger('click')
+    wrapper.vm.error = 'an error has occured trying to update the song'
+    expect(wrapper.vm.error).toEqual('an error has occured trying to update the song')
+  })
+  it('contains an error prop equal to null', () => {
+    moxios.uninstall()
+    const wrapper = shallow(EditSong, {
+      propsData: {
+        song: {}
+      },
+      store,
+      localVue
+    })
+    expect(wrapper.vm.error).toEqual(null)
   })
 })
