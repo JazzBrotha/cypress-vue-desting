90b5f657b50c7b37f0b539b9be3a9a337b9a9fce
JazzBrotha <mattiasostblom@protonmail.com>
2018-01-30 19:59:40 +0100

    Fix component paths

--- client/test/specs/Lyrics.spec.js
+++ client/test/specs/Lyrics.spec.js
@@ -1,9 +1,13 @@
 import { shallow } from '@vue/test-utils'
-import Lyrics from '../../src/components/globals/Lyrics.vue'
+import Lyrics from '../../src/components/ViewSong/Lyrics.vue'
 
 describe('Lyrics.vue', () => {
   it('contains song in props', () => {
-    const wrapper = shallow(Lyrics)
+    const wrapper = shallow(Lyrics, {
+      propsData: {
+        song: {}
+      }
+    })
     expect(wrapper.props()).toHaveProperty('song')
   })
 })
--- client/test/specs/YouTube.spec.js
+++ client/test/specs/YouTube.spec.js
@@ -1,5 +1,5 @@
 import { shallow } from '@vue/test-utils'
-import YouTube from '../../src/components/globals/YouTube.vue'
+import YouTube from '../../src/components/ViewSong/YouTube.vue'
 
 describe('YouTube.vue', () => {
   it('contains youtubeId in props', () => {
