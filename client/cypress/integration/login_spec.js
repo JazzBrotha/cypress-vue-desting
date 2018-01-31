a74752c38801ffaf0d6d84794e87f0bd077fe223
pappagnu <andreas@klonad.se>
2018-01-31 18:29:39 +0100

    successful login with status of 200

--- client/cypress/integration/login_spec.js
+++ client/cypress/integration/login_spec.js
@@ -4,65 +4,59 @@ describe('Login component', () => {
   beforeEach(() => {
     cy.visit(clientRoute)
   })
-  it('it returns correct error message if no input values are given', () => {
-    cy.server({
-      method: 'POST',
-      status: 400,
-      body: {},
-      response: {
-        error: 'No password provided'
-      }
-    })
-    cy.route('/login/')
-    //   .as('postLogin')
-    // cy.wait('@postLogin')
-    // cy.get('@postLogin')
-      .its('response')
-      .should('have.property', 'error', 'No password provided')
-  })
-  it('it returns correct error message if no email is provided', () => {
-    cy.server({
-      method: 'POST',
-      status: 400,
-      body: {
-        password: 'superpassword'
-      },
-      response: {
-        error: 'No email provided'
-      }
-    })
-    cy.route('/login/')
-      .its('response')
-      .should('have.property', 'error', 'No email provided')
-  })
-  it('it returns correct error message if no password is provided', () => {
-    cy.server({
-      method: 'POST',
-      status: 400,
-      body: {
-        email: 'testuser@test.se'
-      },
-      response: {
-        error: 'No password provided'
-      }
-    })
-    cy.route('/login/')
-      .its('response')
-      .should('have.property', 'error', 'No password provided')
-  })
-  // it('responds with status 403 if no user was found', () => {
-  //   cy.request({
+  // it('it returns correct error message if no input values are given', () => {
+  //   cy.server({
   //     method: 'POST',
-  //     url: serverRoute,
-  //     failOnStatusCode: false,
-  //     body: {
-  //       email: 'testuser@test.se',
-  //       password: 'superpassword'
+  //     status: 400,
+  //     body: {},
+  //     response: {
+  //       error: 'No password provided'
   //     }
   //   })
-
-  //   .then((response) => {
-  //     expect(response.status).to.eq(403)
-  //   })
+  //   cy.route('/login/')
+  //     .its('response')
+  //     .should('have.property', 'error', 'No password provided')
   // })
+  // it('it returns correct error message if no email is provided', () => {
+  //   cy.server({
+  //     method: 'POST',
+  //     status: 400,
+  //     body: {
+  //       password: 'superpassword'
+  //     },
+  //     response: {
+  //       error: 'No email provided'
+  //     }
+  //   })
+  //   cy.route('/login/')
+  //     .its('response')
+  //     .should('have.property', 'error', 'No email provided')
+  // })
+  // it('it returns correct error message if no password is provided', () => {
+  //   cy.server({
+  //     method: 'POST',
+  //     status: 400,
+  //     body: {
+  //       email: 'testuser@test.se'
+  //     },
+  //     response: {
+  //       error: 'No password provided'
+  //     }
+  //   })
+  //   cy.route('/login/')
+  //     .its('response')
+  //     .should('have.property', 'error', 'No password provided')
+  // })
+  it('logs in user', () => {
+    cy.server()
+    cy.route('POST', '/login')
+      .as('postLogin')
+    cy.get('#email-input').type('testing@gmail.com')
+    cy.get('#password-input').type('12345678')
+    cy.get('#login-btn').click()
+    cy.wait('@postLogin')
+    cy.get('@postLogin').then(response => {
+      expect(response.status).to.eq(200)
+    })
+  })
 })
