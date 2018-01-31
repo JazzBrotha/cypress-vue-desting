1f7d148b1ca646659a0104ad10b58fdfe5a36601
pappagnu <andreas@klonad.se>
2018-01-31 18:48:10 +0100

    small fix for login e2e tests

--- client/cypress/integration/login_spec.js
+++ client/cypress/integration/login_spec.js
@@ -4,50 +4,50 @@ describe('Login component', () => {
   beforeEach(() => {
     cy.visit(clientRoute)
   })
-  // it('it returns correct error message if no input values are given', () => {
-  //   cy.server({
-  //     method: 'POST',
-  //     status: 400,
-  //     body: {},
-  //     response: {
-  //       error: 'No password provided'
-  //     }
-  //   })
-  //   cy.route('/login/')
-  //     .its('response')
-  //     .should('have.property', 'error', 'No password provided')
-  // })
-  // it('it returns correct error message if no email is provided', () => {
-  //   cy.server({
-  //     method: 'POST',
-  //     status: 400,
-  //     body: {
-  //       password: 'superpassword'
-  //     },
-  //     response: {
-  //       error: 'No email provided'
-  //     }
-  //   })
-  //   cy.route('/login/')
-  //     .its('response')
-  //     .should('have.property', 'error', 'No email provided')
-  // })
-  // it('it returns correct error message if no password is provided', () => {
-  //   cy.server({
-  //     method: 'POST',
-  //     status: 400,
-  //     body: {
-  //       email: 'testuser@test.se'
-  //     },
-  //     response: {
-  //       error: 'No password provided'
-  //     }
-  //   })
-  //   cy.route('/login/')
-  //     .its('response')
-  //     .should('have.property', 'error', 'No password provided')
-  // })
-  it('logs in user', () => {
+  it('it returns correct error message if no input values are given', () => {
+    cy.server({
+      method: 'POST',
+      status: 400,
+      body: {},
+      response: {
+        error: 'No password provided'
+      }
+    })
+    cy.route('/login/')
+      .its('response')
+      .should('have.property', 'error', 'No password provided')
+  })
+  it('it returns correct error message if no email is provided', () => {
+    cy.server({
+      method: 'POST',
+      status: 400,
+      body: {
+        password: 'superpassword'
+      },
+      response: {
+        error: 'No email provided'
+      }
+    })
+    cy.route('/login/')
+      .its('response')
+      .should('have.property', 'error', 'No email provided')
+  })
+  it('it returns correct error message if no password is provided', () => {
+    cy.server({
+      method: 'POST',
+      status: 400,
+      body: {
+        email: 'testuser@test.se'
+      },
+      response: {
+        error: 'No password provided'
+      }
+    })
+    cy.route('/login/')
+      .its('response')
+      .should('have.property', 'error', 'No password provided')
+  })
+  it('logs in a user successfully', () => {
     cy.server()
     cy.route('POST', '/login')
       .as('postLogin')
